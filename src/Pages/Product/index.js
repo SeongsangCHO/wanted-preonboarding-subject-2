73ce9a8d67bdfe1083c9e2a4988b8f78b04d2f75
WOOBEEN <hwb0218@gmail.com>
2021-07-30 17:40:55 +0900

    feat : product 랜덤 기능 추가

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -1,30 +1,80 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const BASE_URL = 'http://localhost:3000';
+
 class ProductPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentProduct: {},
+      productArrayLength: 0,
+      randomNum: -1
+    };
   }
 
   componentWillMount() {}
 
-  componentDidMount() {}
+  handleBtnClick = () => {
+    this.setState({ currentProduct: this.getOneProduct() })
+  } 
+
+  randomPicker = () => {
+    const randomNum = Math.floor(Math.random() * this.state.productArrayLength); 
+    if (randomNum === this.state.randomNum) {
+      this.randomPicker();  
+    }
+    this.setState({ randomNum });
+    return randomNum;
+  }
+
+  getOneProduct = () => {
+    const randomNum = this.randomPicker();
+    const loadedMockData = localStorage.getItem('mockData');
+    const parsedMockData = JSON.parse(loadedMockData);
+    return parsedMockData[randomNum];
+  }
+
+  componentDidMount = async () => {
+    const fetchMockData = await fetch(`${BASE_URL}/MockData/data.json`);
+    const productData = await fetchMockData.json();
+    this.setState({ productArrayLength: productData.length })
+    localStorage.setItem('mockData', JSON.stringify(productData));
+
+    const currentProduct = this.getOneProduct();
+    this.setState({ currentProduct });
+    localStorage.setItem('watchedHistory', JSON.stringify(currentProduct));  
+  }
 
   componentWillReceiveProps(nextProps) {}
 
-  shouldComponentUpdate(nextProps, nextState) {}
+  shouldComponentUpdate(nextProps, nextState) {
+    const productStateChange = this.state.currentProduct !== nextState.currentProduct;
+    return productStateChange;
+  }
 
   componentWillUpdate(nextProps, nextState) {}
 
-  componentDidUpdate(prevProps, prevState) {}
+  componentDidUpdate(prevProps, prevState) {
+    console.log(prevState);
+  }
 
   componentWillUnmount() {}
 
   render() {
-    return <div>Product Page</div>;
+    const { currentProduct } = this.state;
+    console.log(currentProduct);
+    return (
+      <div>
+        {currentProduct.title}
+        <button onClick={this.handleBtnClick}>돌려돌려!</button>
+      </div>
+    );
   }
 }
 
-ProductPage.propTypes = {};
+ProductPage.propTypes = {
+  productArrayLength: PropTypes.object
+};
 
 export default ProductPage;

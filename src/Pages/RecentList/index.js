3b461549382273f09b3f62747bbae6da3a15cbb7
imhjlov <wprkftngh@gmail.com>
2021-07-30 17:03:14 +0900

    style: recentList 페이지 스타일

--- src/Pages/RecentList/index.js
+++ src/Pages/RecentList/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Navbar from "../../Components/Navbar";
+import {ListContainer, ItemWrapper,ItemTitle,ItemInfo,Button} from "./style";
 
 class RecentListPage extends Component {
   constructor(props) {
@@ -23,12 +24,22 @@ class RecentListPage extends Component {
 
   render() {
     return (
-      <div>
-        recentListPage
-        <Navbar></Navbar>
-        <div>관심상품</div>
-        <div>상품정보</div>
-      </div>
+      <ListContainer>
+        <ItemWrapper>
+          <ItemTitle><p>중고 나이키 테아 흰검 245 30000원</p></ItemTitle>
+          <ItemInfo>
+            <Button isHasInteresting={false}><img alt="*ㅅ*"/><span>관심없음등록</span></Button>
+            <p>30,000원</p>
+          </ItemInfo>
+        </ItemWrapper>
+        <ItemWrapper>
+        <ItemTitle><p>중고 나이키 테아 흰검 245 30000원</p></ItemTitle>
+            <ItemInfo>
+              <Button isHasInteresting={false}><img alt="*ㅅ*"/><span>관심없음등록</span></Button>
+              <p>30,000원</p>
+            </ItemInfo>
+        </ItemWrapper>
+      </ListContainer>
     );
   }
 }

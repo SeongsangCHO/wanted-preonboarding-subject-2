6875097ba687ebd3d862b2fa2d05f0909dd12829
WOOBEEN <hwb0218@gmail.com>
2021-07-31 14:31:44 +0900

    feat : 랜덤 버튼, 예외처리, 관심없음 기능 추가

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -2,84 +2,166 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Header from "../../Components/Header";
 import { Container } from "../../Components/Header/style";
-
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = "http://localhost:3000";
 
 class ProductPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProduct: {},
-      productArrayLength: 0,
-      randomNum: -1
+      currentPrintData: {},
+      currentTitle: "",
+      printableData: [],
+      recentListData: [],
     };
   }
 
-  componentWillMount() {}
+  setRecentListNoInterestingData = (isInteresting) => {
+    const recentLocal = this.getLocalStorageData("recentList");
+    if (
+      recentLocal.some(
+        (item) => item?.title === this.state.currentPrintData.title
+      )
+    ) {
+      this.setLocalStorageData("recentList", [
+        ...recentLocal.filter(
+          (item) => item.title !== this.state.currentPrintData.title
+        ),
+        {
+          ...this.state.currentPrintData,
+          time: Date.now(),
+          isInteresting: isInteresting,
+        },
+      ]);
+    } else {
+      this.setLocalStorageData("recentList", [
+        ...recentLocal,
+        {
+          ...this.state.currentPrintData,
+          time: Date.now(),
+          isInteresting: isInteresting,
+        },
+      ]);
+    }
+    this.setLocalStorageData("printableLocalData", this.state.printableData);
+  };
 
-  handleBtnClick = () => {
-    this.setState({ currentProduct: this.getOneProduct() })
-  } 
+  noInteresting = () => {
+    const { printableData, currentTitle } = this.state;
+    this.setState(
+      (prev) => ({
+        recentListData: [],
+      }),
+      () => this.setRecentListNoInterestingData(false)
+    );
+    const filterdPrintableData = printableData.filter(
+      (data) => data.title !== currentTitle
+    );
+    this.setState(
+      {
+        printableData: [...filterdPrintableData],
+      },
+      () => this.randomPicker()
+    );
+  };
 
   randomPicker = () => {
-    const randomNum = Math.floor(Math.random() * this.state.productArrayLength); 
-    if (randomNum === this.state.randomNum) {
-      this.randomPicker();  
+    const { printableData, currentTitle } = this.state;
+
+    const printableLength = printableData.length;
+
+    let randomIndex = Math.floor(Math.random() * printableLength);
+    while (
+      printableLength > 0 &&
+      printableData[randomIndex].title === currentTitle
+    ) {
+      randomIndex = Math.floor(Math.random() * printableLength);
     }
-    this.setState({ randomNum });
-    return randomNum;
+    if (!(randomIndex > printableLength || printableLength === 0)) {
+      this.setState(
+        {
+          currentTitle: printableData[randomIndex].title,
+          currentPrintData: printableData[randomIndex],
+        },
+        () => this.setRecentListNoInterestingData(true)
+      );
+    }
+  };
+
+  getLocalStorageData = (dataName) => {
+    const localData = localStorage.getItem(dataName);
+    if (localData === null) {
+      return [];
+    }
+    return JSON.parse(localData);
+  };
+
+  setLocalStorageData = (itemName, data) => {
+    localStorage.setItem(itemName, JSON.stringify(data));
+  };
+  getApiData = async () => {
+    const fetchApiData = await fetch(`${BASE_URL}/MockData/data.json`);
+    const apiData = await fetchApiData.json();
+    this.setState({
+      printableData: [
+        ...apiData.map((data) => ({ ...data, isInteresting: true })),
+      ],
+    });
+    this.randomPicker();
+  };
+  async componentDidMount() {
+    const localData = await this.getLocalStorageData("printableLocalData");
+    if (localStorage.getItem("printableLocalData") === null) {
+      const fetchApiData = await fetch(`${BASE_URL}/MockData/data.json`);
+      const apiData = await fetchApiData.json();
+      console.log(localData, 'lcocoocco')
+
+      this.setState({
+        printableData: [
+          ...apiData.map((data) => ({ ...data, isInteresting: true })),
+        ],
+      });
+      this.setLocalStorageData("printableLocalData", this.state.printableData);
+    } else if (localData.length === 0) {
+    } else {
+      console.log(localData);
+      this.setState({
+        printableData: [...localData],
+      });
+    }
+    this.randomPicker();
   }
 
-  getOneProduct = () => {
-    const randomNum = this.randomPicker();
-    const loadedMockData = localStorage.getItem('mockData');
-    const parsedMockData = JSON.parse(loadedMockData);
-    return parsedMockData[randomNum];
+  componentWillUnmount() {
+    this.setLocalStorageData("printableLocalData", this.state.printableData);
   }
-
-  componentDidMount = async () => {
-    const fetchMockData = await fetch(`${BASE_URL}/MockData/data.json`);
-    const productData = await fetchMockData.json();
-    this.setState({ productArrayLength: productData.length })
-    localStorage.setItem('mockData', JSON.stringify(productData));
-
-    const currentProduct = this.getOneProduct();
-    this.setState({ currentProduct });
-    localStorage.setItem('watchedHistory', JSON.stringify(currentProduct));  
-  }
-
-  componentWillReceiveProps(nextProps) {}
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const productStateChange = this.state.currentProduct !== nextState.currentProduct;
-    return productStateChange;
-  }
-
-  componentWillUpdate(nextProps, nextState) {}
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log(prevState);
-  }
-
-  componentWillUnmount() {}
 
   render() {
-    const { currentProduct } = this.state;
-    console.log(currentProduct);
+    const { printableData, currentTitle } = this.state;
     return (
       <Container>
         <Header></Header>
-        <div>
-          {currentProduct.title}
-          <button onClick={this.handleBtnClick}>돌려돌려!</button>
-        </div>
+        {printableData.length === 0 ? (
+          <div>너가 다 관심없다 해따 가서 지워와~</div>
+        ) : (
+          <div>
+            {currentTitle}
+            <button
+              disabled={printableData.length === 1 ? true : false}
+              onClick={() => this.randomPicker()}
+            >
+              돌려돌려!
+            </button>
+            <button onClick={() => this.noInteresting()}>관심없어</button>
+          </div>
+        )}
       </Container>
     );
   }
 }
 
 ProductPage.propTypes = {
-  productArrayLength: PropTypes.object
+  currentPrintData: PropTypes.object,
+  currentTitle: PropTypes.string,
 };
 
 export default ProductPage;
